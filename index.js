1a6ec4d9b712569d174fddcd7dbced18e662d46d
Glitch (messengerbothoroscopo) <none>
2018-02-03 16:50:46 +0000

    🍿👦 Checkpoint .env:637990/4 index.js:637990/2

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = PAGE_ACCESS_TOKEN;//"cadenamia";
+  let VERIFY_TOKEN = PAGE_ACCESS_TOKEN;
     
   // Parse the query params
   let mode = req.query['hub.mode'];
