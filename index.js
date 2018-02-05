5d44722c8de38bd1a0c2845aaf2f034b94fa176b
Glitch (messengerbothoroscopo) <none>
2018-02-05 16:25:05 +0000

    👲🍂 Checkpoint .env:637990/0 index.js:637990/34

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/webhook', (req, res) => {
    
     // Iterates over each entry - there may be multiple if batched ???
     body.entry.forEach(function(entry) {//???
-      if(!entry.messaging){console.log("undef");return;}
+      if(!entry.messaging){console.log("received something without messaging object.");return;}
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
