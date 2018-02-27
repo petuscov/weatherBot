acc7a033ea73d58f635d1f92afb50fb21c95f42b
Petuscov <petuscov@gmail.com>
2018-02-27 19:54:07 +0100

    fixed file arrays export

--- routes/helpers/basicArrays.js
+++ routes/helpers/basicArrays.js
@@ -3,8 +3,11 @@ var no =  ["no","no!","negativo","nanai","niet","nop","nope"]
 var si =  ["si","yes!","yes","afirmativo","equilicua","yep","yay","yay!"];
 var ayuda = ["ayuda","help","/help","info","/info"];
 var tiempo = ["pronóstico","pronostico","tiempo","weather","/weather"];
+
 module.exports = {
 	cancel = cancel,
 	no = no,
-	si = si
+	si = si,
+	ayuda = ayuda,
+	tiempo = tiempo
 }
