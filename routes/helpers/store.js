651833bac38c21fe8878798a115b660ee488c005
Petuscov <petuscov@gmail.com>
2018-02-28 22:43:08 +0100

    city save pre2

--- routes/helpers/store.js
+++ routes/helpers/store.js
@@ -7,6 +7,8 @@ var store = {
 	}
 };
 
-module.exports = store; 
+module.exports = {
+	store: store
+}; 
 //teniendo en cuenta que guardamos muy pocos datos y muy simples, no hace falta una store 'compleja'. (e.g redux)
 //futuro igual es interesante llevar a sql. (por ahora en memoria nos sirve)
