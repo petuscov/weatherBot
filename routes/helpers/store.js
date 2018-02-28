c46cdee4b40ca7b54d79595b390a4141996b828e
Petuscov <petuscov@gmail.com>
2018-02-28 22:39:49 +0100

    city save pre1

---
+++ routes/helpers/store.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var store = {
+	"exampleUser1":{ //exampleUser1 = psid de facebook
+		"language": "ES", //currently supported ES, EN.
+		"city": "Bilbao"
+	}
+};
+
+module.exports = store; 
+//teniendo en cuenta que guardamos muy pocos datos y muy simples, no hace falta una store 'compleja'. (e.g redux)
+//futuro igual es interesante llevar a sql. (por ahora en memoria nos sirve)
