bf1c109f9b43e551b082281bdc68aaea1ceeab71
Petuscov <petuscov@gmail.com>
2018-03-01 19:55:18 +0100

    change data management21

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -144,7 +144,11 @@ var guardarCiudad = (convo,city)=>{
       }
     }
   };
-  if(store.getData(convo.userId).city!==city){
+  if(store.getData(convo.userId)){
+    if(store.getData(convo.userId).city!==city){
+      convo.ask(question, answer);
+    }
+  }else{
     convo.ask(question, answer);
   }
 }
