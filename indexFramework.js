e45d7770294182585c00cf696fb4e4f5431176d5
Julen <petuscov@gmail.com>
2018-03-03 19:31:25 +0100

    fixin fails3

--- indexFramework.js
+++ indexFramework.js
@@ -80,7 +80,7 @@ bot.hear(basicArrays.language,(payload,chat)=>{
 bot.on('message', (payload, chat, data) => {
   if(!data.captured){
     var language = store.getData(payload.sender.id).language || "EN";
-    var message = languageRoute[language].notHandled;
+    var message = language[language].notHandled;
     var options = { typing: true };
     chat.say(message, options)
   };
